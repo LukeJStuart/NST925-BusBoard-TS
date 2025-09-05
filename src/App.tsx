681742c8901058bcc451f7b0711df6c4564da0f1
Luke Stuart <luke.stuart@softwire.com>
2025-09-05 15:47:58 +0100

    Part 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import getLiveArrivals from "../backend/fetchArrivals";
+
 function App() {
+  const [arrivalsData, setArrivalsData] = useState<string>("");
+
+  const handleFetchArrivals = async () => {
+    try {
+      const response = await getLiveArrivals();
+      setArrivalsData(JSON.stringify(response, null, 2));
+    } catch {
+      setArrivalsData("Error fetching arrivals");
+    }
+  };
+
   return (
-        <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4"
-        >BusBoard</h1>
-  )
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-center text-cyan-600 m-4">
+        BusBoard
+      </h1>
+      <button onClick={handleFetchArrivals}>Get Arrivals</button>
+      <div>
+        <pre>{arrivalsData}</pre>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
